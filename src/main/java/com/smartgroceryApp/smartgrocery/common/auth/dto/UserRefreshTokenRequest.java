2cdcf27badda33f32fe3543cf13efebfbcba7724
Ahmedabdelaziz77 <102679746+Ahmedabdelaziz77@users.noreply.github.com>
2026-04-05 00:35:06 +0200

    remove auth package from common added by mistake and add it to the project

--- src/main/java/com/smartgroceryApp/smartgrocery/common/auth/dto/UserRefreshTokenRequest.java
+++
@@ -1,8 +0,0 @@
-package com.smartgroceryApp.smartgrocery.common.auth.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public class UserRefreshTokenRequest {
-    @NotBlank(message = "refresh token is required!")
-    public String refreshToken;
-}
