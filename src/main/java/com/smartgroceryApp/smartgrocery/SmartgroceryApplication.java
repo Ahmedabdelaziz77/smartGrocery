f95735dd8b0964924acad9ed34033d3048058f5f
Ahmedabdelaziz77 <zozahmed770@gmail.com>
2026-04-04 20:06:21 +0200

    Init project

---
+++ src/main/java/com/smartgroceryApp/smartgrocery/SmartgroceryApplication.java
@@ -0,0 +1,13 @@
+package com.smartgroceryApp.smartgrocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgroceryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgroceryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartgroceryApp/smartgrocery/SmartgroceryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgroceryApp.smartgrocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgroceryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
