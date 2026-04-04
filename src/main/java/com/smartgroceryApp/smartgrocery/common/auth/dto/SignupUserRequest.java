e81d94a6923cbfab928f8ef5f6d538324e07730c
Ahmedabdelaziz77 <zozahmed770@gmail.com>
2026-04-04 23:20:34 +0200

    add login and signup endpoints and their dto's

---
+++ src/main/java/com/smartgroceryApp/smartgrocery/common/auth/dto/SignupUserRequest.java
@@ -0,0 +1,26 @@
+package com.smartgroceryApp.smartgrocery.common.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class SignupUserRequest {
+    @NotBlank(message = "full name is required!")
+    public String fullName;
+
+
+    @NotBlank(message = "email is required!")
+    @Email(message = "email is invalid")
+    @Size(max = 255, message = "email mustn't exceed 255 chars!")
+    public String email;
+
+
+    @NotBlank(message = "password is required !")
+    @Size(min = 6, max = 50, message = "password must be between 6 and 50 chars!")
+    public String password;
+}
